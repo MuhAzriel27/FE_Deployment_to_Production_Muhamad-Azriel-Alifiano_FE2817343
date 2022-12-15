a07f69124652f3237743f1fb909ab1b1f4e90ccc
Muhamad Azriel Alifiano <l200200027@student.ums.ac.id>
2022-12-15 15:07:31 +0700

    commit assignment

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Muhamad Azriel Alifiano" // TODO: replace this
+const studentId = "FE2817343" // TODO: replace this
+
+export { url, studentName, studentId }
